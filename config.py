66273c116e5f6325f2b191f624e2c3ee5521b539
guneet-makin <guneet_makin@yahoo.com>
2025-07-22 23:42:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 # Simulated secret key
-AWS_SECRET_ACCESS_KEY = "AKIA1234567890ABCD"
+# FAKE AWS KEY for testing
+aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
+aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 DB_PASSWORD = "SuperSecret123!"
 secret = "SECRET-GUNEET-LEAK-12345"
